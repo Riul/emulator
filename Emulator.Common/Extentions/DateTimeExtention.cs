6796530c02f04ef19ca123df77e70d73957e945f
Phito <phito41@gmail.com>
2013-04-28 18:01:05 +0200

    Implemented unix timestamp

---
+++ Emulator.Common/Extentions/DateTimeExtention.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emulator.Common.Extentions
+{
+    public static class DateTimeExtention
+    {
+        public static int ToUnixTimestamp(this DateTime dateTime)
+        {
+            return (int)(dateTime - new DateTime(1970, 1, 1).ToLocalTime()).TotalSeconds;
+        }
+    }
+}
