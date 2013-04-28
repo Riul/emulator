d64a48b978c5f6a2311394e97314183a2b315f10
Phito <phito41@gmail.com>
2013-04-28 21:36:42 +0200

    Fixed translation error.

---
+++ Emulator.Game/Approach/Character/CharacterCreationManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Emulator.Common.Network.Dispatching;
+using Emulator.Common.Protocol.Net.Messages.Game.Character.Creation;
+using Emulator.Game.Network;
+
+namespace Emulator.Game.Approach.Character
+{
+    public class CharacterCreationManager
+    {
+        private readonly GameClient client;
+
+        public CharacterCreationManager(GameClient client)
+        {
+            this.client = client;
+            client.Dispatcher.Register(this);
+        }
+
+        [MessageHandler(CharacterNameSuggestionRequestMessage.ID)]
+        public void HandleCharacterNameSuggestionRequestMessage(CharacterNameSuggestionRequestMessage message)
+        {
+            //todo
+            client.Send(new CharacterNameSuggestionFailureMessage(0));
+        }
+
+        [MessageHandler(CharacterCreationRequestMessage.ID)]
+        public void HandleCharacterCreationRequestMessage(CharacterCreationRequestMessage message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
